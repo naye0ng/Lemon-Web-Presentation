1e3ba64dded2296e7515fda27feba902704dc913
naye0ng <nayeong_e@naver.com>
2020-07-08 14:48:41 +0900

    [ITTF-39] 개발 환경 설정 : Kakao IDS

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 // css import
-import '../css/style.css';
+import '../css/common.css';
+import '../css/lemon.css';
 
-console.log('setting success!!');
+// 자바스크립트에서 파일 삽입
+// import check from '../img/check.png';
+
+// function addImage () {
+//   const img = document.createElement('img');
+//   img.src = check;
+//   img.width = 300;
+//   const body = document.querySelector('body');
+//   body.appendChild(img);
+// }
+
+// addImage();
