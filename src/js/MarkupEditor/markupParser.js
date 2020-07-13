2167088ba5c3badfd3a699cd72c36699d515f418
kelley-j <kelley.j@kakaocorp.com>
2020-07-13 19:11:51 +0900

    [ITTF-250] 마크업 파서 예외 처리
    - 허용하지 않는 태그 무시
    - 사용자가 에디터 상에서 닫지 않은 태그 제어 ex) (<p , <p></) => <p></p>

--- src/js/MarkupEditor/markupParser.js
+++ src/js/MarkupEditor/markupParser.js
@@ -1,34 +1,67 @@
-const TEXT_NODE = 3;
-const ERROR = 'parsererror';
+const TEXT = 3;
 
-const isChild = node => {
-  if (node.nodeType === TEXT_NODE && node.nodeValue.trim() === '') {
-    return false;
-  }
-  if (node.tagName === ERROR) {
-    return false;
-  }
+const ALLOW_TAGS = [
+  'SLIDE', 'DIV', 'TITLE', 'P', 'IMAGE', 'IMG',
+  'LIST', 'LI', 'OL', 'BOLD', 'B', 'ITALIC', 'I', '#text',
+];
+
+const TAGS = {
+  SLIDE: 'DIV',
+  TITLE: 'P',
+  TEXT: 'P',
+  IMAGE: 'IMG',
+  LIST: 'LI',
+  BOLD: 'B',
+  ITALIC: 'I',
+};
+
+const isNode = node => {
+  if (!ALLOW_TAGS.includes(node.nodeName)) return false;
+  if (node.nodeType === TEXT && node.nodeValue.trim() === '') return false;
+
   return true;
 };
 
+const attrParser = (name, value) => {
+  if (name === ':color') return ['style', `color: ${value};`];
+  if (name === ':background-color') return ['style', `background-color: ${value};`];
+  if (name === ':size') return ['style', `font-size: ${value};`];
+  if (name === ':bold') return ['style', value ? 'font-weight: 600;' : ''];
+  if (name === ':italic') return ['style', value ? 'font-style: italic; ' : ''];
+  if (name === ':width') return ['style', `width: ${value}%;`];
+  if (name === ':height') return ['style', `height: ${value}%;`];
+  if (name === ':float') return ['style', `float: ${value};`];
+  if (name === ':decorator') return ['style', `list-style: ${value === 'number' ? 'decimal' : value};`];
+  // TODO : animation class 추가로 정의
+  return [name, value];
+};
 
 const markupParser = node => {
-  if (node.nodeType === TEXT_NODE) {
+  if (node.nodeType === TEXT) {
     return node.nodeValue;
   }
 
   const parsedObj = {
-    tagName: node.tagName,
-    attrs: {},
+    tagName: TAGS[node.tagName] || node.tagName,
+    attrs: {
+      class: '',
+      style: '',
+    },
     children: Array.prototype.filter
-      .call(node.childNodes, child => isChild(child))
+      .call(node.childNodes, child => isNode(child))
       .map(child => markupParser(child)),
   };
 
-  for (const {nodeName, nodeValue} of node.attributes) {
-    parsedObj.attrs[nodeName] = nodeValue;
+  for (let i = 0; i < node.attributes.length || 0; i++) {
+    let nodeName; let nodeValue;
+    ({nodeName, nodeValue} = node.attributes[i]);
+    if (nodeValue) {
+      if (nodeName !== 'class' && nodeName !== 'style') {
+        [nodeName, nodeValue] = attrParser(nodeName, nodeValue);
+      }
+      parsedObj.attrs[nodeName] += `${nodeValue} `;
+    }
   }
-
   return parsedObj;
 };
 
