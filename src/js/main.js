13299acbedf7913faf47f7f13a5f055acd875b49
naye0ng <nayeong_e@naver.com>
2020-07-13 15:36:48 +0900

    [ITTF-235] 슬라이드 및 에디터 스타일 정의

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,5 @@
-// css import
+import './MarkupParser/markupParser';
+
 import '../css/common.css';
 import '../css/lemon.css';
 
-// 자바스크립트에서 파일 삽입
-// import check from '../img/check.png';
-
-// function addImage () {
-//   const img = document.createElement('img');
-//   img.src = check;
-//   img.width = 300;
-//   const body = document.querySelector('body');
-//   body.appendChild(img);
-// }
-
-// addImage();
