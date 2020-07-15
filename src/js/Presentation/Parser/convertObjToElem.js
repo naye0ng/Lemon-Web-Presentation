c8846ac37a29b0be0bd48add745e0d6b3b2e963e
naye0ng <nayeong_e@naver.com>
2020-07-15 10:07:30 +0900

    [ITTF-287] 코드를 리펙터링한다.
    - index.html : 태그 중복 제거
    - convertObjToElem.js : 함수 이름 변경
    - markupParser.js : parsedObj 분리 및 markupParser 함수 수정

--- src/js/Presentation/Parser/convertObjToElem.js
+++ src/js/Presentation/Parser/convertObjToElem.js
@@ -1,4 +1,4 @@
-const renderElement = ({tag, attrs, children}) => {
+const bindElem = ({tag, attrs, children}) => {
   const el = document.createElement(tag);
 
   for (const [name, value] of Object.entries(attrs)) {
@@ -7,15 +7,15 @@ const renderElement = ({tag, attrs, children}) => {
 
   for (const child of children) {
     if (attrs.class === 'slide-container ' && typeof child === 'string') continue;
-    el.appendChild(createElement(child));
+    el.appendChild(convertObjToElem(child));
   }
 
   return el;
 };
 
-const createElement = node => {
+const convertObjToElem = node => {
   if (typeof node === 'string') return document.createTextNode(node);
-  return renderElement(node);
+  return bindElem(node);
 };
 
-export default createElement;
+export default convertObjToElem;
