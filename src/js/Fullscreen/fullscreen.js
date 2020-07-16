7000b1d107dd6f5a2cf86f5bdc4aca01988ebfae
naye0ng <nayeong_e@naver.com>
2020-07-16 13:30:07 +0900

    [ITTF-280] 발표(Fullscreen) 모드를 지원한다.
    [ITTF-305] 발표 모드의 레이아웃을 구성한다.
     - 캐러셀로 발표 화면 구성
    [ITTF-309] 버튼과 키보드로 슬라이드 페이지를 전환할 수 있다.
    - 키보드 방향키 추가

--- src/js/Fullscreen/fullscreen.js
+++ src/js/Fullscreen/fullscreen.js
@@ -1,33 +1,81 @@
 class Fullscreen {
   constructor (domElemFunc) {
-    // 전체모드에서 들어갈 버튼 껍데기? 레이아웃
-    // this.closedBtn = '';
-    this.domElemFunc = domElemFunc;
+    this.isFullscreen = false;
+    this.fullscreenWrapper = document.querySelector('#fullscreen');
 
+    this.carousel = null;
+    this.carouselSlideNumber = 0;
+    this.slideCount = 0;
+
+    this.domElemFunc = domElemFunc;
     this.init();
   }
 
   init () {
+    this.bindToggleFullscreen();
+    this.bindArrowController();
+
     document.querySelector('button.fullscreen-btn').addEventListener('click', () => {
-      this.openFullscreen();
+      if (!this.isFullscreen) {
+        this.startFullscreen();
+      }
     });
   }
 
-  openFullscreen () {
-    // TODO : fullscreen 껍대기 만들어서 추가하기
-    const fullscreenEl = this.domElemFunc();
-    if (!fullscreenEl.childElementCount) {
+  bindToggleFullscreen () {
+    this.fullscreenWrapper.addEventListener('webkitfullscreenchange', () => {
+      this.isFullscreen = !this.isFullscreen;
+    });
+    this.fullscreenWrapper.addEventListener('mozfullscreenchange', () => {
+      this.isFullscreen = !this.isFullscreen;
+    });
+    this.fullscreenWrapper.addEventListener('msfullscreenchange', () => {
+      this.isFullscreen = !this.isFullscreen;
+    });
+    this.fullscreenWrapper.addEventListener('fullscreenchange', () => {
+      this.isFullscreen = !this.isFullscreen;
+    });
+  }
+
+  bindArrowController () {
+    this.fullscreenWrapper.addEventListener('keydown', ({key}) => {
+      if (this.isFullscreen) {
+        if (key === 'ArrowLeft' && this.carouselSlideNumber > 0) {
+          this.carouselSlideNumber -= 1;
+          this.carousel.style.marginLeft = `${-100 * this.carouselSlideNumber}vw`;
+        } else if (key === 'ArrowRight' && this.carouselSlideNumber < this.slideCount - 1) {
+          this.carouselSlideNumber += 1;
+          this.carousel.style.marginLeft = `${-100 * this.carouselSlideNumber}vw`;
+        }
+      }
+    });
+  }
+
+  startFullscreen () {
+    const slideNodes = this.domElemFunc();
+    this.slideCount = slideNodes.childElementCount;
+    if (!this.slideCount) {
       alert('작성된 슬라이드가 없습니다. \n슬라이드를 만들어주세요!');
     } else {
       const renderFullscreen =
-        fullscreenEl.requestFullScreen ||
-        fullscreenEl.webkitRequestFullScreen ||
-        fullscreenEl.mozRequestFullScreen ||
-        fullscreenEl.msRequestFullscreen;
+        this.fullscreenWrapper.requestFullScreen ||
+        this.fullscreenWrapper.webkitRequestFullScreen ||
+        this.fullscreenWrapper.mozRequestFullScreen ||
+        this.fullscreenWrapper.msRequestFullscreen;
+
+      const firstChildEl = this.fullscreenWrapper.firstChild;
+      if (firstChildEl) {
+        this.fullscreenWrapper.removeChild(firstChildEl);
+      }
+
+      this.fullscreenWrapper.appendChild(slideNodes.cloneNode(true));
+
+      this.carousel = document.querySelector('#fullscreen > .slide-container');
+      this.carousel.style.width = `${100 * this.slideCount}vw`;
 
       // TODO : IE에서 테스트!
       if (renderFullscreen) {
-        renderFullscreen.call(fullscreenEl);
+        renderFullscreen.call(this.fullscreenWrapper);
       }
     }
   }
