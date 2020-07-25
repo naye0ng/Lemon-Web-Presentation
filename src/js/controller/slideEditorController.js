d2dc7d3e1b2b8144c591ddeebd6d2c47c0af8778
naye0ng <nayeong_e@naver.com>
2020-07-25 21:15:44 +0900

    [ITTF-372] : 4주차 코드를 리팩토링한다.
    - slideEditorController.js
    에디터의 텍스트 입력으로 슬라이드 관련 작업(CRUD)을 할 수 있었던 전과 다르게, 버튼으로 슬라이드를 추가, 삭제, 업데이트할 수 있도록 변경함

--- src/js/controller/slideEditorController.js
+++ src/js/controller/slideEditorController.js
@@ -6,42 +6,91 @@ import updateVDOM from '../module/vDOM/updateVDOM';
 class SlideEditorController {
   constructor () {
     this.view = new SlideEditorView(this);
-    this.editingSlideIndex = -1;
 
     this.slides = [];
+    this.slidesID = [];
     this.slideSize = 0;
 
+    this.editingSlideIndex = -1;
     this.createSlide();
   }
 
   createSlide () {
-    const textareaValue = '';
-    const DOMTree = convertStringToDOM(textareaValue);
+    const ID = `${Math.random()}`;
+    const DOMTree = convertStringToDOM();
     const slide = markupParser(DOMTree);
-    const note = '';
 
-    this.slides.push({
-      textareaValue,
-      DOMTree,
-      slide, // 슬라이드 속성변화는 여기!
-      note,
+    DOMTree.addEventListener('click', () => {
+      this.focusOnNthSlide(this.slidesID.indexOf(ID));
     });
 
-    this.editingSlideIndex = this.slideSize;
+    this.editingSlideIndex += 1;
     this.slideSize += 1;
 
-    this.view.$textarea.value = textareaValue;
-    this.view.render(DOMTree, this.view.$viewer);
+    this.slides.splice(this.editingSlideIndex, 0, {
+      textareaValue: '',
+      note: '',
+      DOMTree,
+      slide, //  TODO : 버튼으로 슬라이드 속성변화 시키는 건 여기?
+    });
+
+    this.slidesID.splice(this.editingSlideIndex, 0, ID);
+    this.view.viewer.render(DOMTree, this.view.viewer.$slideContainer, this.editingSlideIndex);
+    this.changeTextareaValue();
   }
 
   updateSlide (newTextareaValue) {
+    if (this.editingSlideIndex < 0) {
+      this.changeTextareaValue();
+      return alert('슬라이드가 존재하지 않습니다.\n슬라이드를 생성해주세요!');
+    }
+
     const {slide, DOMTree} = this.slides[this.editingSlideIndex];
     const newSlide = markupParser(convertStringToDOM(newTextareaValue));
     const patch = updateVDOM(slide, newSlide);
 
     this.slides[this.editingSlideIndex].slide = newSlide;
+    this.slides[this.editingSlideIndex].textareaValue = newTextareaValue;
     patch(DOMTree);
   }
+
+  deleteSlide () {
+    if (this.editingSlideIndex < 0) return;
+    const [{DOMTree}] = this.slides.splice(this.editingSlideIndex, 1);
+    this.slidesID.splice(this.editingSlideIndex, 1);
+    this.editingSlideIndex -= 1;
+    DOMTree.remove();
+    this.changeTextareaValue();
+  }
+
+  focusOnBeforeSlide () {
+    if (this.editingSlideIndex <= 0) return;
+    this.editingSlideIndex -= 1;
+    this.changeTextareaValue();
+  }
+
+  focusOnNextSlide () {
+    if (this.editingSlideIndex >= this.slideSize - 1) return;
+    this.editingSlideIndex += 1;
+    this.changeTextareaValue();
+  }
+
+  focusOnNthSlide (n) {
+    if (n < 0 || n >= this.slideSize) return;
+    this.editingSlideIndex = n;
+    this.changeTextareaValue();
+  }
+
+  changeTextareaValue () {
+    if (this.editingSlideIndex < 0) {
+      this.view.editor.$textarea.value = '';
+      // TODO : 슬라이드 추가하는 버튼 깜빡깜빡 효과!!
+      return;
+    }
+    this.view.editor.$textarea.value = this.slides[this.editingSlideIndex].textareaValue;
+  }
+
+  // TODO : 드래그 이벤트로 changeSlideOrder 추가
 }
 
 
--- src/js/view/slideEditorView.js
+++ src/js/view/slideEditorView.js
@@ -8,6 +8,7 @@ class SlideEditorView extends View {
     super();
     this.controller = controller;
 
+    // TODO : 바로 view를 사용할 수 있도록 할 것!
     this.viewer = new Viewer();
     this.toolbar = new Toolbar();
     this.editor = new Editor();
@@ -28,7 +29,10 @@ class SlideEditorView extends View {
       this.controller.updateSlide(event.target.value);
     });
     this.toolbar.$view.addEventListener('click', event => {
-      console.log(event.target);
+      if (event.target.id === 'delete') return this.controller.deleteSlide();
+      if (event.target.id === 'create') return this.controller.createSlide();
+      if (event.target.id === 'before') return this.controller.focusOnBeforeSlide();
+      if (event.target.id === 'next') return this.controller.focusOnNextSlide();
     });
   }
 }
