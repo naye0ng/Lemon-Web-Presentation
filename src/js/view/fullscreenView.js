7c6300e07f7f1294c4a1f81c9f1fd72c1202e262
kelley-j <kelley.j@kakaocorp.com>
2020-08-04 20:32:23 +0900

    [ITTF-369] 풀스크린 하단 메뉴바로 프레젠테이션 페이지를 조절할 수 있다.
    - 메뉴바 추가
    - 마우스 포인터 추가

--- src/js/view/fullscreenView.js
+++ src/js/view/fullscreenView.js
@@ -7,7 +7,7 @@ class FullscreenView extends View {
     this.controller = controller;
 
     this.$fullscreen = createCustomElement('div', {id: 'fullscreen'});
-    this.$fullscreen.innerHTML = '<div id="fullscreen-menu" class="active"><div class="fullscreen-toolber"><div class="slide-toolber"><button id="before"></button><div class="input-slide-number"><input id="show-slide-number" type="number" max="1" min="1" value="1"></div><button id="next"></button></div><button id="pointer">포인터</button><button id="helper">발표자 노트</button></div></div><div id="fullscreen-contents"></div>';
+    this.$fullscreen.innerHTML = '<div id="fullscreen-menu" class="active"><div class="fullscreen-toolber"><div class="slide-toolber"><button id="before"></button><div class="input-slide-number"><input id="show-slide-number" type="number" max="1" min="1" value="1"></div><button id="next"></button></div><button id="pointer">포인터</button><button id="helper">발표자 노트</button></div></div><div id="fullscreen-contents"></div><div id="mouse-pointer"></div>';
   }
 
   init () {
@@ -16,8 +16,15 @@ class FullscreenView extends View {
   }
 
   initListeners () {
-    document.addEventListener('keydown', ({key}) => this.controller.arrowKeyHandler(key));
+    document.addEventListener('keyup', e => this.controller.documentEventHandler(e));
+    document.addEventListener('mousemove', e => this.controller.documentEventHandler(e));
+    document.addEventListener('mouseenter', e => this.controller.documentEventHandler(e));
+    document.addEventListener('mouseleave', e => this.controller.documentEventHandler(e));
     this.$fullscreen.addEventListener('click', ({target}) => this.controller.eventHandler(target));
+    this.$fullscreen.addEventListener('keyup', e => {
+      e.stopPropagation();
+      this.controller.eventHandler(e.target);
+    });
   }
 }
 
