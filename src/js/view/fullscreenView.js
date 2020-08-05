ead48e438a0cea3df6ca2586a01524c66542c16f
kelley-j <kelley.j@kakaocorp.com>
2020-08-05 14:50:18 +0900

    [ITTF-373] 팝업창으로 프레젠테이션 도구 모음 창을 활성화 할 수 있다 : 팝업창 추가

--- src/js/view/fullscreenView.js
+++ src/js/view/fullscreenView.js
@@ -7,7 +7,12 @@ class FullscreenView extends View {
     this.controller = controller;
 
     this.$fullscreen = createCustomElement('div', {id: 'fullscreen'});
-    this.$fullscreen.innerHTML = '<div id="fullscreen-menu" class="active"><div class="fullscreen-toolber"><div class="slide-toolber"><button id="before"></button><div class="input-slide-number"><input id="show-slide-number" type="number" max="1" min="1" value="1"></div><button id="next"></button></div><button id="pointer">포인터</button><button id="helper">발표자 노트</button></div></div><div id="fullscreen-contents"></div><div id="mouse-pointer"></div>';
+    this.$fullscreenMenu = createCustomElement('div', {id: 'fullscreen-menu'});
+    this.$fullscreenMenu.innerHTML = '<div class="fullscreen-toolber"><div class="slide-toolber"><button id="before"></button><div class="input-slide-number"><input id="pt-number" type="number" max="1" min="1" value="1"></div><button id="next"></button></div><button id="pointer">포인터</button><button id="helper">발표자 노트</button></div>';
+
+    this.$fullscreenContents = createCustomElement('div', {id: 'fullscreen-contents'});
+    this.$mousePointer = createCustomElement('div', {id: 'mouse-pointer'});
+    this.$fullscreen.append(this.$fullscreenMenu, this.$fullscreenContents, this.$mousePointer);
   }
 
   init () {
@@ -16,15 +21,14 @@ class FullscreenView extends View {
   }
 
   initListeners () {
-    document.addEventListener('keyup', e => this.controller.documentEventHandler(e));
-    document.addEventListener('mousemove', e => this.controller.documentEventHandler(e));
-    document.addEventListener('mouseenter', e => this.controller.documentEventHandler(e));
-    document.addEventListener('mouseleave', e => this.controller.documentEventHandler(e));
-    this.$fullscreen.addEventListener('click', ({target}) => this.controller.eventHandler(target));
     this.$fullscreen.addEventListener('keyup', e => {
       e.stopPropagation();
-      this.controller.eventHandler(e.target);
+      this.controller.eventHandler(e);
     });
+    this.$fullscreen.addEventListener('mousemove', e => this.controller.eventHandler(e));
+    this.$fullscreen.addEventListener('mouseenter', e => this.controller.eventHandler(e));
+    this.$fullscreen.addEventListener('mouseleave', e => this.controller.eventHandler(e));
+    this.$fullscreen.addEventListener('click', e => this.controller.eventHandler(e));
   }
 }
 
