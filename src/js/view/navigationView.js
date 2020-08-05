6218fb0373148e0f65e459fa0a86e45dce223af1
naye0ng <nayeong_e@naver.com>
2020-08-05 19:50:30 +0900

    [ITTF-373] 팝업창으로 프레젠테이션 도구 모음 창을 활성화 할 수 있다.
    - 팝업에서 프레젠테이션 제어
    - 타이머 기능 미완성

--- src/js/view/navigationView.js
+++ src/js/view/navigationView.js
@@ -7,7 +7,7 @@ class NavigationView extends View {
     super();
     this.controller = controller;
     this.$navigation = createCustomElement('div', {class: 'navigation'});
-    this.$navigation.innerHTML = '<div class="lemon-logo"></div><div><button id="first-slide" class="fullscreen-btn">슬라이드 쇼</button><button id="current-slide" class="fullscreen-btn">현재 슬라이드부터 쇼</button><button id="pt-helper" class="fullscreen-btn">발표자 노트</button></div>';
+    this.$navigation.innerHTML = '<div class="lemon-logo"></div><div><button id="first-slide" class="fullscreen-btn">슬라이드 쇼</button><button id="current-slide" class="fullscreen-btn">현재 슬라이드부터 쇼</button><button id="helper-popup" class="fullscreen-btn">발표자 노트</button></div>';
   }
 
   init () {
