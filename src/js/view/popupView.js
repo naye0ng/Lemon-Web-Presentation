a5719c1d6829ce1c1c29b6df535851200b426730
naye0ng <nayeong_e@naver.com>
2020-08-07 10:10:23 +0900

    [ITTF-665] 프로젝트를 리팩토링한다.
    - popup view 분리
    - view render 함수 app.js에서 분리 및 전체적인 호출 순서 변경

--- src/js/view/popupView.js
+++ src/js/view/popupView.js
@@ -1,27 +1,29 @@
-
-import {createElement} from '../Utils/DOMConstructor';
-import {POPUP_STYLE, POPUP_HTML} from './popup/popup_contents';
-
-
 const popupView = () => {
-  const $popup = createElement('div', {class: 'navigation'});
-  const style = createElement('style');
-
-  const render = function () {
-    $popup.innerHTML = POPUP_HTML;
-    style.innerHTML = POPUP_STYLE;
-  };
-
-  const bindPopupEvent = (type, handler) => {
-    $popup.addEventListener(type, e => handler(e));
-  };
-
-  return {
-    render,
-    bindPopupEvent,
-    $popup,
-    style,
-  };
+  const render = ({timer, slideNumber, slideSize, note}) => `<div class="presentaion-helper">
+    <div class="timer-and-viewer">
+      <div id="timer">
+        <div id="time-view">${timer}</div>
+        <div class="buttons">
+          <button id="start-timer">시작</button>
+          <button id="stop-timer">정지</button>
+          <button id="reset-timer">초기화</button>
+        </div>
+      </div>
+      <div id="viewer">
+      </div>
+    </div>
+    <div class="controller-and-note">
+      <div id="slide-controller">
+        <div id="total">슬라이드 ${slideNumber} / ${slideSize}</div>
+        <div class="buttons">
+          <button id="before">이전</button>
+          <button id="next">다음</button>
+        </div>
+      </div>
+      <div id="presentation-note">${note}</div>
+    </div>
+    </div>`;
+  return {render};
 };
 
 export default popupView;
