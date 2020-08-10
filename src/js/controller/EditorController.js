493f0266437b103543b9df4cd3bd2cddae963f4b
kelley-j <kelley.j@kakaocorp.com>
2020-08-10 14:11:51 +0900

    [ITTF-370] 에디터 및 프레젠테이션 관련 추가 기능을 구현한다.
    - storage 데이터 불러올 때, 에디터 툴바가 색상을 감지하지 못하는 문제 해결 : rgb => hex과정에서 0 => 00으로 표기!

--- src/js/controller/EditorController.js
+++ src/js/controller/EditorController.js
@@ -117,7 +117,11 @@ const editorController = model => {
   const rgbToHex = rgbText => {
     if (!rgbText) return;
     const rgb = rgbText.replace(/[^%,.\d]/g, '').split(',');
-    const hex = rgb.map(dec => parseInt(dec).toString(16));
+    const hex = rgb.map(dec => {
+      const hexColor = parseInt(dec).toString(16);
+      if (hexColor.length < 2) return `0${parseInt(dec).toString(16)}`;
+      return parseInt(dec).toString(16);
+    });
     return `#${hex.join('')}`;
   };
 
